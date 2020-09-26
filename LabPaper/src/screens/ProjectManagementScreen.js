026405b1a3b1c7af8714f50cd47062e109393925
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2020-09-26 16:59:50 +0700

    ...

--- LabPaper/src/screens/ProjectManagementScreen.js
+++ LabPaper/src/screens/ProjectManagementScreen.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect}  from 'react'
 import { Image, Platform , StyleSheet, View } from 'react-native'
 import { Appbar, Button, Text } from 'react-native-paper'
+import { DataTable } from 'react-native-paper'
 
 import io from 'socket.io-client'
 
@@ -22,10 +23,33 @@ const ProjectManagementScreen = ({ navigation }) => {
 				<Appbar.Content title="Projects Management" subtitle={'MienTay Ecosystem'} />
 				<Appbar.Action icon={MORE_ICON} onPress={() => {}} />
 			</Appbar.Header>
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				{
-					projects.map((v,i)=><div key={i}>{v.project_name}</div>)
-				}
+			<View>
+				<DataTable>
+					<DataTable.Header>
+						<DataTable.Title>Project</DataTable.Title>
+						<DataTable.Title>Status</DataTable.Title>
+						<DataTable.Title>Act</DataTable.Title>
+					</DataTable.Header>
+					{
+						projects!=null ? projects.map((v,i) => 
+							<DataTable.Row key={i}>
+								<DataTable.Cell>{v.project_name}</DataTable.Cell>
+								<DataTable.Cell>-</DataTable.Cell>
+								<DataTable.Cell>
+									<Button icon="camera"/>
+								</DataTable.Cell>
+							</DataTable.Row>
+						) : null
+					}
+				 	<DataTable.Pagination
+						page={1}
+						numberOfPages={3}
+						onPageChange={page => {
+						console.log(page);
+						}}
+						label="m-n of z"
+					/>
+				</DataTable>
 			</View>
 		</View>
 	)
